217d60805ca45fbda387a02f6329b401a3b545d6
ynhi16 <nguyenthiynhi010601@gmail.com>
2021-12-16 21:14:53 +0700

    admin_danhmuc

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,12 @@ use App\Http\Controllers;
 use Illuminate\Support\Facades\Redirect;
 class ProductController extends Controller
 {
-    
+    public function add_product() {
+        return view('admin.add-product');
+    }
+    public function all_product() {
+        return view('admin.all-product');
+        
+    }
 
 }
