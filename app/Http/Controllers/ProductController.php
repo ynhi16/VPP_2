9a5e0c96e5aeeea6973680b5ab8d4f21a54b4c05
ynhi16 <nguyenthiynhi010601@gmail.com>
2021-12-16 20:20:13 +0700

    admin_danhmuc

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
+use Session;
+use App\Http\Controllers;
+use Illuminate\Support\Facades\Redirect;
 class ProductController extends Controller
 {
-    public function add_product() {
-        return view('admin.add-product');
-    }
-    public function all_product() {
-        return view('admin.all-product');
-    }
+    
+
 }
