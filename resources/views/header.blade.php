b09fa2277e559767e28cf0365c3d7f6358277b06
y nhi <nguyenthiynhi010601@gmail.com>
2022-05-07 16:34:37 +0700

    trangchu

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,64 +1,70 @@
 <style>
-    .li-2 {
-        position: relative;
-        list-style: none;
-        display: inline-block;
-        padding: 0;
-        margin: 0;
-        width: 100px;
-        background-color: #FDF9F9;
-    }
+.li-2 {
+    position: relative;
+    list-style: none;
+    display: inline-block;
+    padding: 0;
+    margin: 0;
+    width: 100px;
+    background-color: #FDF9F9;
+}
 
-    .li-2-a {
-        display: block;
-        text-decoration: none;
-        padding: 0 10px;
-        color: #454545;
-        background-color: #FDF9F9;
+.li-2-a {
+    display: block;
+    text-decoration: none;
+    padding: 0 10px;
+    color: #454545;
+    background-color: #FDF9F9;
 
-    }
+}
 
-    .ul-2 {
-        position: absolute;
-        display: none;
-        margin: 0;
-        padding: 0;
-    }
+.ul-2 {
+    position: absolute;
+    display: none;
+    margin: 0;
+    padding: 0;
+}
 
-    .li-1:hover>.ul-2 {
-        display: block;
-    }
+.li-1:hover>.ul-2 {
+    display: block;
+}
 
-    .li-1 {
-        margin-top: 10px;
-        width: 100px;
-    }
+.li-1 {
+    margin-top: 10px;
+    width: 100px;
+}
 
-    .input-search {
+.input-search {
 
-        width: 100%;
+    width: 100%;
 
-        border-radius: 10px;
+    border-radius: 20px;
 
-        padding: 8px;
+    padding: 8px;
+    border-color: #198754;
 
 
-
-        }
+}
 </style>
 <div class="navbar_style">
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-            <div class="d-flex flex-column bd-highlight mb-3" style="height: 100px">
+    <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="d-flex justify-content-between" style="width: 100%; height: 50px;">
+            <div class="mt-2 ">
                 <a class="navbar-brand" href="{{URL::to('/')}}">
                     <h1 class="title">Sahami</h1>
                 </a>
             </div>
-           
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="d-flex flex-column bd-highlight mb-3">
+
+
+            <?php $nguoidung_id = Session::get('nguoidung_id') ?>
+            @if($nguoidung_id)
+            <div>
                 <div class="p-2 bd-highlight ">
                     <form action="{{URL::to('/timkiem')}}" autocomplete="off" method="post" class="d-flex">
                         {{csrf_field()}}
@@ -68,22 +74,9 @@
                         </button>
                     </form>
                 </div>
-            
-                <div class="p-2">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        @foreach ($categories as $category)
-                        <li class="nav-item me-2">
-                            <a class="nav-link" href="{{URL::to('/danhmuc/'. $category -> maDM )}}">{{ $category -> tenDM }}</a>
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <?php $nguoidung_id = Session::get('nguoidung_id') ?>
-            @if($nguoidung_id)
-            <div class="d-flex align-items-start bd-highlight mb-3" style="height: 100px">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li><a class="navbar_right" href="#"><img src="{{asset('public/frontend/img/avtkhachhang.png')}}" style="width: 30px; height: 30px;"></a></li>
+                    <li><a class="navbar_right" href="#"><img src="{{asset('public/frontend/img/avtkhachhang.png')}}"
+                                style="width: 30px; height: 30px;"></a></li>
                     <li class="nav-item li-1"><span>
                             <?php 
                                 $hoten = Session::get('nguoidung_name');
@@ -92,7 +85,7 @@
                                 $name = $fullname[$count-2] . " " . $fullname[$count-1];
                                 echo $name;
                             ?>
-                            </span>
+                        </span>
                         <ul class="ul-2">
                             <li class="li-2"><a href="{{URL::to('/canhan')}}" class="li-2-a">Cá nhân</a></li>
                             <li class="li-2"><a href="{{URL::to('/yeuthich')}}" class="li-2-a">Yêu thích</a></li>
@@ -104,16 +97,26 @@
                     </li>
                     <button type="button" class="btn me-2">
                         <a href="{{URL::to('/giohang')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-cart3" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                             </svg>
                         </a>
                     </button>
                 </ul>
             </div>
             @else
-
-            <div class="d-flex align-items-start bd-highlight mb-3" style="height: 100px">
+            <div class="p-2" style="width: 95%;">
+                <form action="{{URL::to('/timkiem')}}" autocomplete="off" method="post" class="d-flex">
+                    {{csrf_field()}}
+                    <input class="input-search me-2" type="search" placeholder="Tìm kiếm" name="tk" id="key">
+                    <button class="btn btn-outline-success" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div style="width:21%" class="pt-2">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="navbar_right" href="{{URL::to('/dangnhap')}}">Đăng Nhập</a>
@@ -125,7 +128,17 @@
             </div>
             @endif
         </div>
-        
     </nav>
+    <div>
+        <ul class="d-flex justify-content-between ps-0">
+            @foreach ($categories as $category)
+            <li class="nav-item me-2 navbar-nav ">
+                <a class="nav-link text-secondary fs-6"
+                    href="{{URL::to('/danhmuc/'. $category -> maDM )}}">{{ $category -> tenDM }}</a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
     <div id="search_ajax" class="container mb-4"></div>
+
 </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Layout</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('public/frontend/css/layout.css')}}">
     <link rel="stylesheet" href="{{asset('public/frontend/css/trangchu.css')}}">
     <link rel="stylesheet" href="{{asset('public/frontend/css/khachhang.css')}}">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 
@@ -28,41 +31,42 @@
     @include('footer')
 
     <script type="text/javascript">
-        $(document).ready(function() {
-            $('#key').keyup(function() { //bắt sự kiện keyup khi người dùng gõ từ khóa tim kiếm
-                var query = $(this).val(); //lấy gía trị ng dùng gõ
-                if (query != '') //kiểm tra khác rỗng thì thực hiện đoạn lệnh bên dưới
-                {
-                    var _token = $('input[name="_token"]').val(); // token để mã hóa dữ liệu
-                    $.ajax({
-                        url: "{{url('/auto-ajax')}}", // đường dẫn khi gửi dữ liệu đi 'search' là tên route mình đặt bạn mở route lên xem là hiểu nó là cái j.
-                        method: "POST", // phương thức gửi dữ liệu.
-                        data: {
-                            query: query,
-                            _token: _token
-                        },
-                        success: function(data) { //dữ liệu nhận về
-                            $('#search_ajax').fadeIn();
-                            $('#search_ajax').html(data); //nhận dữ liệu dạng html và gán vào cặp thẻ có id là countryList
-                        },
-                        error: function(xhr) {
-                            console.log(xhr.responseText);
-                        }
-                    });
-                }
-            });
+    $(document).ready(function() {
+        $('#key').keyup(function() { //bắt sự kiện keyup khi người dùng gõ từ khóa tim kiếm
+            var query = $(this).val(); //lấy gía trị ng dùng gõ
+            if (query != '') //kiểm tra khác rỗng thì thực hiện đoạn lệnh bên dưới
+            {
+                var _token = $('input[name="_token"]').val(); // token để mã hóa dữ liệu
+                $.ajax({
+                    url: "{{url('/auto-ajax')}}", // đường dẫn khi gửi dữ liệu đi 'search' là tên route mình đặt bạn mở route lên xem là hiểu nó là cái j.
+                    method: "POST", // phương thức gửi dữ liệu.
+                    data: {
+                        query: query,
+                        _token: _token
+                    },
+                    success: function(data) { //dữ liệu nhận về
+                        $('#search_ajax').fadeIn();
+                        $('#search_ajax').html(
+                            data
+                        ); //nhận dữ liệu dạng html và gán vào cặp thẻ có id là countryList
+                    },
+                    error: function(xhr) {
+                        console.log(xhr.responseText);
+                    }
+                });
+            }
         });
-        $(document).on('click', '.search', function() {
-            $('#key').val($(this).text());
-            $('#search_ajax').fadeOut();
-        });
+    });
+    $(document).on('click', '.search', function() {
+        $('#key').val($(this).text());
+        $('#search_ajax').fadeOut();
+    });
     </script>
 
     <script>
+    var usd = document.getElementById("vnd_to_usd").value;
 
-        var usd = document.getElementById("vnd_to_usd").value;
-
-        paypal.Button.render({
+    paypal.Button.render({
 
         // Configure environment
 
@@ -70,9 +74,9 @@
 
         client: {
 
-        sandbox: 'demo_sandbox_client_id',
+            sandbox: 'demo_sandbox_client_id',
 
-        production: 'demo_production_client_id'
+            production: 'demo_production_client_id'
 
         },
 
@@ -82,11 +86,11 @@
 
         style: {
 
-        size: 'small',
+            size: 'small',
 
-        color: 'gold',
+            color: 'gold',
 
-        shape: 'pill',
+            shape: 'pill',
 
         },
         // Enable Pay Now checkout flow (optional)
@@ -103,13 +107,13 @@
 
                 transactions: [{
 
-                amount: {
+                    amount: {
 
-                    total: `${usd}`,
+                        total: `${usd}`,
 
-                    currency: 'USD'
+                        currency: 'USD'
 
-                }
+                    }
 
                 }]
 
@@ -131,9 +135,9 @@
 
         }
 
-        }, '#paypal-button');
+    }, '#paypal-button');
     </script>
-    
+
 
 </body>
 
